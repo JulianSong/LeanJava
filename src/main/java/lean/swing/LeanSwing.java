7368528eb8af4cba7fc28651f8d98132a94a40b2
Junliang.Song <songjl1@yonghui.cn>
2022-10-20 20:06:55 +0800

    feat: add button to lean swing

--- src/main/java/lean/swing/LeanSwing.java
+++ src/main/java/lean/swing/LeanSwing.java
@@ -1,13 +1,26 @@
 package lean.swing;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LeanSwing extends JFrame {
+public class LeanSwing extends JFrame implements ActionListener {
     public static void  leanSwing() {
-        JFrame jf = new JFrame();
+        LeanSwing jf = new LeanSwing();
         jf.setBounds(100,100,400,400);
-        JButton button = new JButton();
+
+        JPanel jp = new JPanel();
+        jf.getContentPane().add(jp);
+        JButton button = new JButton("Ok");
+        button.setBounds(0,0,40,10);
+        button.addActionListener(jf);
+        jp.add(button);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
     }
 }
